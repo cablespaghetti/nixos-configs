daaf338d28291374497675176a0afd7479fc77b1
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2023-08-20 17:14:31 +0100

    Add tmux and bmon

--- common/configuration.nix
+++ common/configuration.nix
@@ -32,7 +32,7 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [wget htop vim curl git];
+  environment.systemPackages = with pkgs; [wget htop vim curl git tmux bmon];
 
   home-manager = {
     useGlobalPkgs = true;
