cc517b451d7807a2e83ffcbb4084ecfa5c9686c5
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2023-08-22 09:28:12 +0100

    Allow caddy to listen on 443

--- chonky/joplin.nix
+++ chonky/joplin.nix
@@ -18,6 +18,8 @@
     };
   };
   config.environment.systemPackages = with pkgs; [cloudflare-caddy];
+  # because we're using a custom caddy package
+  systemd.services.caddy.serviceConfig.AmbientCapabilities = "CAP_NET_BIND_SERVICE";
   config.services.caddy = {
     enable = true;
     package = pkgs.cloudflare-caddy;
