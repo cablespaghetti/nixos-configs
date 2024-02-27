b8de2da04687039c09a4e412d44c04fd7305324d
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-02-27 21:57:06 +0000

    Give up on ssh key checking

--- hosts/chonky/syncoid.nix
+++ hosts/chonky/syncoid.nix
@@ -8,12 +8,16 @@
     enable = true;
     commonArgs = [
       "--no-sync-snap"
+      "--sshoption=StrictHostKeyChecking=off"
     ];
-    user = "root";
     commands = {
-      runningcafe-web1 = {
+      runningcafe-web1-postgres16 = {
         source = "root@runningcafe-web1:rpool/postgres16";
         target = "bigdata/runningcafe-web1/postgres16";
+      };
+      runningcafe-web1-redis-storage = {
+        source = "root@runningcafe-web1:rpool/redis-storage";
+        target = "bigdata/runningcafe-web1/redis-storage";
       };
     };
   };
