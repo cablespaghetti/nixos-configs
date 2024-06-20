4e4fc9622acce554d62c7d89bae8d2f86c148438
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-06-20 21:12:33 +0100

    Remove commented out stuff

--- roles/laptops/configuration.nix
+++ roles/laptops/configuration.nix
@@ -9,9 +9,6 @@
   inputs,
   ...
 }: {
-  # Enable pretty booting
-  boot.plymouth.enable = true;
-
   # Enable networking
   networking.networkmanager.enable = true;
 
@@ -56,46 +53,10 @@
   services.pipewire = {
     enable = true;
     alsa.enable = true;
-    alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
   hardware.opengl.enable = true;
-
-  # Styling
-  #  fonts = {
-  #    fonts = with pkgs; [
-  #      noto-fonts
-  #      noto-fonts-emoji
-  #    ];
-  #
-  #    fontconfig = {
-  #      # Fixes pixelation
-  #      antialias = true;
-  #
-  #      # Fixes antialiasing blur
-  #      hinting = {
-  #        enable = true;
-  #        style = "hintfull"; # no difference
-  #        autohint = true; # no difference
-  #      };
-  #
-  #      subpixel = {
-  #        # Makes it bolder
-  #        rgba = "rgb";
-  #        lcdfilter = "default"; # no difference
-  #      };
-  #    };
-  #  };
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.sam = {
@@ -104,48 +65,11 @@
     extraGroups = ["networkmanager" "wheel" "docker"];
   };
 
-  #nixpkgs.config.permittedInsecurePackages = [
-  #  "openssl-1.1.1u"
-  #];
-
-  #virtualisation.docker = {
-  #  enable = true;
-  #};
-
-  # $ nix search wget
   environment.systemPackages = with pkgs; [
     imagemagick
-
     librewolf
     gnomeExtensions.caffeine
-    #joplin-desktop
+    joplin-desktop
     gimp
-    #beeper
-    tilix
-    #rclone
-    #libreoffice
-
-    # I am DevOps
-    #awscli2
-    #opentofu
-    #kubectl
-    #kubectx
-    #fluxcd
-    #kubernetes-helm
-    #nmap
-    #python3
-    #python311Packages.pip
-    #pipenv
-    #hugo
-    #(vscode-with-extensions.override {
-    #  vscodeExtensions = with vscode-extensions; [
-    #    bbenoist.nix
-    #    ms-python.python
-    #    ms-azuretools.vscode-docker
-    #    redhat.vscode-yaml
-    #    hashicorp.terraform
-    #    kamadorueda.alejandra
-    #  ];
-    #})
   ];
 }
