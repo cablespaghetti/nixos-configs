f11af5e1185f533dfe40132740eee6879434b8b0
Sam Weston <11150054+cablespaghetti@users.noreply.github.com>
2024-06-20 21:32:51 +0100

    Begin home manager rabbithole

--- hosts/snail/configuration.nix
+++ hosts/snail/configuration.nix
@@ -16,4 +16,25 @@
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
+
+  home-manager = {
+    users.sam = {
+      home.sessionVariables = {
+        EDITOR = "vim";
+      };
+      programs = {
+        zsh.enable = true;
+        starship.enable = true;
+        librewolf = {
+          enable = true;
+          settings = {
+            "identity.fxaccounts.enabled" = true;
+            "privacy.clearOnShutdown.history" = false;
+	    "privacy.clearOnShutdown.downloads" = false;
+            "services.sync.engine.passwords" = false; 
+          };
+        };
+      };
+    };
+  };
 }
